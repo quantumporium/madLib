2b1e3390cfc2f23c64b5e51f09f14bbc98b1f10c
quantumporium <quantum[at]porium>
2022-07-03 15:46:30 -0700

    first commit

---
+++ madLib.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    | author:           quantumporium   |
+    | email:            quantum@email   |
+    | name software:    madLib (for py) |
+    -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+    -> Example usage
+    choose a noun:
+    choose a verb:
+    choose an adjective: 
+
+    ***** Here you mad lib! *****
+    There were some time an {noun} who was
+    {verb} and he like a really {adjective} object.
+    He like is so much that .......
+
+    -=-=-=- functinality -=-=-=-
+    [ ] Have 5 multiples stories
+        -> Each story have is unique ammount of part of speech (noun, verb, etc)
+        -> Make the story comme up at random
+    [ ] Get the input (part of speech) from the user
+        -> stored them in an list/array
+        -> Tell how much noun he will need to give (You will need to enter 3 nouns ...)
+    [ ] Add the input(part of speech) to the predefined story
+        -> Each input need to get use
+
+    -=-=-=- testing -=-=-=-
+    [ ] ????
+
+    -=-=-=- note -=-=-=-
+    - Each story will need to be its own function (can't find another way)
+    - Testing function might be difficult (a lot of them will be non-pure function)
+    - I should maybe do this in another file 
+
+"""
+
+## function
+
+
+
+
+
+## main Program loop
