cf526340983c6000cf78278f05b262c1239fecb6
quantumporium <quantum[at]porium>
2022-07-06 00:53:55 -0700

    created first story function, make prototyp for object representation of story, exploratory code

---
+++ README.md
@@ -0,0 +1,22 @@
+# mad libs generator in python (madLib)
+Terminal based [mad libs](https://en.wikipedia.org/wiki/Mad_Libs") generator. Made and implemented with
+python.
+
+# Content table
+- Installation
+- Usage
+- Testing
+- License
+
+# Installation
+You can install this project either by using the graphical interface of github or by fetching/cloning it with the command line. 
+
+```sh
+git clone https://github.com/quantumporium/madLib	# clone with github command line
+
+```
+
+
+# License
+This project use the [GPL.V2](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html") license.
+
--- madLib.py
+++ madLib.py
@@ -34,11 +34,44 @@
     - I should maybe do this in another file 
 
 """
+# data structure
+class story:
+    def __init__(self, nbNoun, nbAdj, nbVerb, nbAdv, storyFunction):
+        self.nbNoun = nbNoun
+        self.nbAdj  = nbAdj
+        self.nbVerb = nbVerb
+        self.nbAdv = nbAdv
+        self.storyFunction = storyFunction
+        
+        return
 
 ## function
+### once upon a time story
+def once_time(noun, adj, verb, adv):
+    """
+    number noun = 3 
+    number adj = 3
+    number verb = 1
+    number adv = 1
+
+    """
+
+    story = """
+Once upon a time, there was a {noun[0]}. The {noun[0]} was very {adj[0]}. He lived in a {adj[1]} town named {noun[1]}. Legend is said that no one was more {adj[0]} and {adj[2]} then {noun[0]} in is town. {noun[0]} add an {noun[2]} and together they go on a {adv[0]} {verb[0]}.
+    """
+    print( story.format(noun = noun, adj = adj, verb = verb, adv = adv) )
+    return
+
+## initialising story object
 
 
+class foo:
+    def __init__(self, name):
+        self.name = name
+        print(self.name)
 
+s = foo(once_time)
 
-
+# s = once_time
+s.name(noun = [1,2, 3] , adj = [1,2,3] , verb = [1], adv = [1]) 
 ## main Program loop
