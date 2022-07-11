eb89e871d1027cfd1f922ebb65907891385c253c
quantumporium <quantum[at]porium>
2022-07-11 04:03:53 -0700

    finished madLib.py, test it at hand

--- madLib.py
+++ madLib.py
@@ -16,7 +16,7 @@
     He like is so much that .......
 
     -=-=-=- functinality -=-=-=-
-    [ ] Have 5 multiples stories
+    [ ] Have 4 multiples stories
         -> Each story have is unique ammount of part of speech (noun, verb, etc)
         -> Make the story comme up at random
     [+] Get the input (part of speech) from the user
@@ -70,10 +70,62 @@ Once upon a time, there was a {noun[0]}. The {noun[0]} was very {adj[0]}. He liv
     print( story.format(noun = noun, adj = adj, verb = verb, adv = adv) )
     return
 
-## initialising story object
-once_upon_time = story(nbNoun = 3, nbAdj = 3, nbVerb = 1, nbAdv = 1, storyFunction = once_time)
+once_upon_time = story( nbNoun = 3, nbAdj = 3, nbVerb = 1, nbAdv = 1, storyFunction = once_time)
+def since_dawn(noun, adj, verb, adv):
+    """
+    name: since the dawn of time
+    nb noun: 4
+    nb adj : 1
+    nv verb: 1
+    nb adv : 
+    """
+    
+    story = """
+Since the dawn of {noun[0]}, the clan of {noun[1]} was in a war with the clan of {noun[2]}. Because they  they didn't like them. This dislike started begin  when two member of the clan of {noun[1]} {verb[0]} a really important item from the leader of the other clan. This item was an {adj[0]} {noun[3]} and was the most precious item of the clan of {noun[2]}. """
+    
+    print( story.format(noun = noun, adj = adj, verb = verb, adv = adv) )
+    return
 
-library_story = [ once_upon_time ]
+since_the_dawn = story( nbNoun = 4, nbAdj = 1, nbVerb = 1, nbAdv = 0, storyFunction = since_dawn )
+def yesterday( noun, adj, verb, adv):
+    """
+    name   : Yesterday
+    nb noun: 3
+    nb adj : 1
+    nb verb: 1
+    nb adv : 2
+    """
+
+    story = """
+Yesterday , {noun[0]} and is {adj[0]} {noun[1]} went on a {verb[0]}. They decided to go for a {verb[0]} because they weren't feeling well. They failed their {noun[2]} exam and felt {adv[0]} bad because of it. But they weren't still totaly defeated by it. They decided that they will work very hard to the next exam in order to {adv[1]} pass it.
+"""
+
+    print( story.format(noun = noun, adj = adj, verb = verb, adv = adv) )
+    return
+## initialising story object
+yesterday_they_did = story( nbNoun = 3, nbAdj = 1, nbVerb = 1, nbAdv = 2, storyFunction = yesterday )
+
+def it_was_day( noun, adj, verb, adv):
+    """
+    noun: it was a day
+    nb noun: 1
+    nb adj : 1
+    nb verb: 1
+    nb adv : 1
+
+    """
+
+    story = """
+It was a {adj[0]} day when I decied to go for a {verb[0]}. I decided to go for a {verb[0]} because I was tired of not feeling good. I know that taking a {verb[0]} cheer me. I saw {noun[0]} during my {verb[0]} and it made me {adv[0]} happy.
+    
+    """
+    print( story.format(noun = noun, adj = adj, verb = verb, adv = adv) )
+    return
+
+it_was_a_day = story( nbNoun = 1, nbAdj = 1, nbVerb = 1, nbAdv = 1, storyFunction = it_was_day)
+
+## array to store the object of the story
+library_story = [ once_upon_time, it_was_a_day, yesterday_they_did, since_the_dawn]
 ## other function - mostly utilities
 def get_part_speech(storyObject):
     userNoun, userAdj, userVerb, userAdv = [], [], [], []
